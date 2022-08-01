dcc4ed897be89cc908c5bbad25db56829d275d4f
rehamalsabbagh <reham.alsabbagh@gmial.com>
2022-08-01 19:33:06 +0100

    Updates

---
+++ precache-manifest.07004fb3d0f0d881a4eac70dfcacab93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c3eea2346de7674651b69ebfaef7c7",
+    "url": "/letshangout_/index.html"
+  },
+  {
+    "revision": "acc4cd9588d5a230d43f",
+    "url": "/letshangout_/static/css/main.4d25e88b.chunk.css"
+  },
+  {
+    "revision": "7f0efb9c8be6e7a823ac",
+    "url": "/letshangout_/static/js/2.4f69ff03.chunk.js"
+  },
+  {
+    "revision": "463cff7a45afc4f228b07df0ab95d6ce",
+    "url": "/letshangout_/static/js/2.4f69ff03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acc4cd9588d5a230d43f",
+    "url": "/letshangout_/static/js/main.a33b530c.chunk.js"
+  },
+  {
+    "revision": "305d487db5dc31bd03c7",
+    "url": "/letshangout_/static/js/runtime-main.5fd046c0.js"
+  }
+]);
